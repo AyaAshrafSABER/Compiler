438bcd76ccec59bae1f60942fee8464027d24861
Aya Osman <ayasaberosman@gmail.com>
2019-03-27 12:40:54 +0200

    Put Minimizer in other Directory

---
+++ error/ErrorHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TARGET on 3/25/2019.
+//
+
+#include "ErrorHandler.h"
---
+++ error/ErrorHandler.h
@@ -0,0 +1,14 @@
+//
+// Created by TARGET on 3/25/2019.
+//
+
+#ifndef COMPILER_ERRORHANDLER_H
+#define COMPILER_ERRORHANDLER_H
+
+
+class ErrorHandler {
+
+};
+
+
+#endif //COMPILER_ERRORHANDLER_H
