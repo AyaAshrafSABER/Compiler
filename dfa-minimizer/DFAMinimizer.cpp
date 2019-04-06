385b57d2e87472d507a65543e559cac86403a63e
Aya Osman <ayasaberosman@gmail.com>
2019-04-06 18:52:27 +0200

    MinDFA without Build new

--- dfa-minimizer/DFAMinimizer.cpp
+++ dfa-minimizer/DFAMinimizer.cpp
@@ -4,21 +4,21 @@
 
 #include "DFAMinimizer.h"
 /* Null, because instance will be initialized on demand. */
-DFAMinimizer* DFAMinimizer::instance = 0;
+
+DFAMinimizer* DFAMinimizer::instance;
 //constructor
 DFAMinimizer::DFAMinimizer()
 {
     this->dfa = DFA::getInstance();
-    this->setFirstPartition(this->Accepted,this->NotAccepted,this->previousPartition);
-    this->partitioning(this->dfa, this->previousPartition, this->nextPartition);
+    this->setFirstPartition(&this->Accepted,&this->NotAccepted,&this->previousPartition);
+    this->partitioning(this->dfa, &this->previousPartition, &this->nextPartition);
     //ToDo construct minimized state table
-    // from pervious partition
-
+    this->buildMinimumeDFA(this->dfa,&this->minimizedTransitionStateTable, &this->previousPartition);
 }
 
 DFAMinimizer* DFAMinimizer::getInstance()
 {
-    if (instance == 0)
+    if (instance == NULL)
     {
         instance = new DFAMinimizer();
     }
@@ -26,37 +26,51 @@ DFAMinimizer* DFAMinimizer::getInstance()
 }
 
 
-void DFAMinimizer:: setFirstPartition(set<Node*> Acc, set<Node*> Not_Acc, vector<set<Node*>> partitions){ //p0
+void DFAMinimizer:: setFirstPartition(set<Node*> *Acc, set<Node*> *Not_Acc, vector<set<Node*>> *partitions){ //p0
     vector<pair<Node*, map<Definition*,Node*>>> transitionStateTable = this->dfa->getDFA();
     vector<pair<Node*, map<Definition*,Node*>>>::iterator ptr ;
     for(auto ptr=transitionStateTable.begin(); ptr!=transitionStateTable.end(); ++ptr) {
-        Node temp = *ptr->first;
-        if (temp.getStatus().compare(N_ACC) == 0) {
-            Not_Acc.insert(&temp);
+        Node* temp = ptr->first;
+        set<Node*>ptrNode;
+        if (temp->getStatus().compare(N_ACC) == 0) {
+           Not_Acc->insert(temp);
         } else {
-            Acc.insert(&temp);
+              Acc->insert(temp);
         }
+
     }
-    partitions.push_back(Acc);
-    partitions.push_back(Not_Acc);
+    cout<<"Accept "<<Acc->size()<<endl;
+    cout<<"NotAccept "<<Not_Acc->size()<<endl;
+    partitions->push_back(*Acc);
+    partitions->push_back(*Not_Acc);
 }
-void DFAMinimizer::partitioning(DFA* dfa, vector<set<Node*>>pre, vector<set<Node*>>next ){
-    this->nextPartition = this->partitioningHelper(dfa, pre);
-    while (!(this->isEqualPartition(pre, next))) {
-        pre = next;
-        next = partitioningHelper(dfa,pre);
+void DFAMinimizer::partitioning(DFA* dfa, vector<set<Node*>> *pre, vector<set<Node*>> *next ){
+
+     this->partitioningHelper(dfa, pre,next);
+     while (!(this->isEqualPartition(pre, next))) {
+        cout<<"size of next partition "<< next->size() <<endl;
+        pre->clear();
+        vector<set<Node*>>::iterator ptr;
+        for(auto ptr = next->begin(); ptr != next->end(); ++ptr) {
+             pre->push_back(*ptr);
+        }
+        partitioningHelper(dfa,pre,next);
     }
-}
-vector<set<Node*>> DFAMinimizer::partitioningHelper(DFA* dfa , vector<set<Node*>>pre){
-    vector<set<Node*>> next;
-    vector<set<Node*>>::iterator ptr ; // loop on every partition in the vector
-    for(auto ptr = pre.begin(); ptr != pre.end(); ++ptr) { //loop on each set on the partition
-        this->distinguishStates(dfa,pre,next,*ptr);
-    }
-    return next;
 
 }
-void DFAMinimizer::distinguishStates(DFA* dfa,vector<set<Node*>> P,vector<set<Node*>> n ,set<Node*> curr){
+void DFAMinimizer::partitioningHelper(DFA* dfa , vector<set<Node*>> *pre ,vector<set<Node*>> *next ){
+    vector<set<Node*>> temp;
+    vector<set<Node*>>::iterator ptr ; // loop on every partition in the vector
+    for(auto ptr = pre->begin(); ptr != pre->end(); ++ptr) { //loop on each set on the partition
+        this->distinguishStates(dfa,pre,&temp,*ptr);
+    }
+    next->clear();
+    for(auto ptr = temp.begin(); ptr != temp.end(); ++ptr) { //add new sets to next
+        next->push_back(*ptr);
+    }
+
+}
+void DFAMinimizer::distinguishStates(DFA* dfa,vector<set<Node*>>* P,vector<set<Node*>> *n ,set<Node*> curr){
     Node* A  = *curr.begin();
     set<Node*>::iterator ptr ; // loop on every state
     set<Node*> set1;
@@ -65,65 +79,92 @@ void DFAMinimizer::distinguishStates(DFA* dfa,vector<set<Node*>> P,vector<set<No
     {
         Node* B  = *ptr;
         if (!areEquivalentStates(dfa,P,A,B)) {
+            cout<<" Not Eq "<<endl;
             set2.insert(B);
         }else {
+            cout<<" Eq  "<<endl;
             set1.insert(B);
         }
     }
     if (set2.size() == 0) {
-        n.push_back(set1);
+        n->push_back(set1);
     }else {
-        n.push_back(set1);
-        n.push_back(set2);
+        n->push_back(set1);
+        n->push_back(set2);
     }
 }
 
 
-bool DFAMinimizer::areEquivalentStates(DFA* dfa , vector<set<Node*>> P, Node* A, Node *B){
+bool DFAMinimizer::areEquivalentStates(DFA* dfa , vector<set<Node*>> *P, Node* A, Node *B){
+    cout<<"Node "<<A->getId();
+    cout<<" Node "<<B->getId();
+    if (A == B) {
+        return true;
+    }
     vector<pair<Node*, map<Definition*,Node*>>> TT = dfa->getDFA();
     vector<pair<Node*, map<Definition*, Node*>>>::iterator ptr ;
     map<Definition*,Node*> a_map;
     map<Definition*,Node*> b_map;
     for(auto ptr = TT.begin(); ptr != TT.end(); ++ptr) {
-        if (ptr->first = A) {
+        if (ptr->first == A) {
             a_map = ptr->second;
-        } else if (ptr->first = B) {
+        }
+         if (ptr->first == B) {
             b_map = ptr->second;
         }
     }
     map<string,Definition*> DT = Definitions_Table::getInstance()->getTable();
-
-    for (const auto& any : DT) {
-        Definition *j = any.second;
+    map<string,Definition*> ::iterator ptr2;
+    int counter = 0;
+    for (auto ptr2 = DT.begin(); ptr2 != DT.end() && counter < DT.size() - 1; ++ptr2) {
+        Definition *j = ptr2->second;
         Node* stateA = a_map[j];
         Node*stateB = b_map[j];
         vector<set<Node*>>::iterator s;
-        for(auto s = P.begin(); s != P.end(); ++s) {
+        for(auto s = P->begin(); s != P->end(); ++s) {
             set<Node*> temp = *s;
             set<Node*>::iterator it1 = temp.find(stateA);
             set<Node*>::iterator it2 = temp.find(stateB);
-            if (it1 != temp.end() && it2 != temp.end()) {
-                return true;
-            } else if (it1 != temp.end() || it2 != temp.end()) {
+            if ((it1 != temp.end() && it2 == temp.end())||(it1 == temp.end() && it2 != temp.end())) {
                 return false;
             }
         }
-
+        counter++;
     }
-    return  false;
+    return  true;
 }
 void DFAMinimizer::printMinimizedDFA(){
 
 
 }
-bool DFAMinimizer::isEqualPartition(vector<set<Node*>> P,vector<set<Node*>> N){
+bool DFAMinimizer::isEqualPartition(vector<set<Node*>> *P,vector<set<Node*>> *N){
 
-    if (P.size() != N.size()){
+    if (P->size() != N->size()){
         return false;
     }
     return true;
 }
 
-std::vector<std::pair<Node*, std::map<Definition*,set<Node*>>>>  DFAMinimizer::getMinimizedDFA(){
+std::vector<std::pair<Node*, std::map<Definition*,Node*>>>  DFAMinimizer::getMinimizedDFA(){
     return this->minimizedTransitionStateTable;
+}
+void DFAMinimizer::buildMinimumeDFA(DFA* dfa,vector<pair<Node*, map<Definition*, Node*>>>*ret, vector<set<Node*>> *nextPartition){
+    //m7taga a3raf el start node mn 3and sara
+//constructe qraph -> create new node for each
+//for each def go to the node in the set give it the pointer to the new node
+    vector<pair<Node*, map<Definition*,Node*>>> transitionStateTable = this->dfa->getDFA();
+    vector<pair<Node*, map<Definition*,Node*>>>::iterator ptrTT ;
+    map<set<Node*>, Node*> minDFA;
+    vector<set<Node*>>::iterator ptrP ;
+    set<Node*>::iterator ptrS;
+    int counter = 0;
+    for (auto ptrP = nextPartition->begin(); ptrP != nextPartition->end(); ++ptrP){
+        Node* node = new Node(counter);
+        auto ptrS = *ptrP->begin();
+        node->setStatus(ptrS->getStatus());
+        minDFA[*ptrP] = node;
+        cout<<"Node "<< counter<<" Status "<<node->getStatus()<<endl;
+        counter++;
+    }
+
 }
